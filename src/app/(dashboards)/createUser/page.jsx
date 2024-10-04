2360021450ab44d2c7985316461a4a7c04135515
mohammad syed hasan <syedhasanmohammad@gmail.com>
2024-10-04 22:23:17 +0600

    added router group concept with (dashboards) router group

---
+++ src/app/(dashboards)/createUser/page.jsx
@@ -0,0 +1,9 @@
+const CreateUserPage = ({}) => {
+  return (
+    <div className="container mx-auto p-6">
+      <h2>CreateUser page</h2>
+    </div>
+  );
+};
+
+export default CreateUserPage;
---
+++ src/app/(dashboards)/dashboard/page.jsx
@@ -0,0 +1,9 @@
+const DashboardPage = ({}) => {
+  return (
+    <div className="container mx-auto p-6">
+      <h2>Dashboard page</h2>
+    </div>
+  );
+};
+
+export default DashboardPage;
