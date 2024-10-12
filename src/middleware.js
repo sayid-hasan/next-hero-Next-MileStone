d782f0d4906d6302cb68f64b87cefe99a0d59b18
mohammad syed hasan <syedhasanmohammad@gmail.com>
2024-10-12 23:51:28 +0600

    if user not logged in and want to go on dashboard redirect him to /login with middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,13 @@
 import { NextResponse } from "next/server";
 
+const user = false;
+
 export const middleware = (request) => {
-  if (request.nextUrl.pathname.startsWith("/about")) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
+  if (!user) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
+  return NextResponse.next();
 };
-// export const config = {
-//   matcher: ["/about"],
-// };
+export const config = {
+  matcher: ["/dashboard", "/services"],
+};
